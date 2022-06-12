41de2a07293cca299221f39a8bcc1d9d9d18807e
kmj4789 <kmj4789@autuni.ac.nz>
2022-06-12 18:31:08 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-nx1evf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-nx1evf)
---
+++ src/app/test-course.ts
@@ -0,0 +1,48 @@
+import { Course } from './courses/seperate';
+
+export const COURSES: Course[] = [
+  {
+    course_id: 1,
+    course_title: 'Web Development',
+    semester: 'one',
+    period: 'Wednesday 4-6pm',
+    lecturer: 'Jian',
+    selectedCourse: this
+
+  },
+  {
+    course_id: 2,
+    course_title: 'Research and Development',
+    semester: 'one',
+    period: 'Tuesday 2-6pm',
+    lecturer: 'Ajit',
+    selectedCourse: this
+
+  },
+  {
+    course_id: 3,
+    course_title: 'CISE',
+    semester: 'one',
+    period: 'wednesday 8-10am',
+    lecturer: 'robert',
+    selectedCourse: this
+
+  },
+  {
+    course_id: 4,
+    course_title: 'HCI',
+    semester: 'one',
+    period: 'Wednesday 10-12pm',
+    lecturer: 'Daniel',
+    selectedCourse: this
+
+  },
+  {
+    course_id: 5,
+    course_title: 'Game Programming',
+    semester: 'two',
+    period: 'Wednesday 4-6pm',
+    lecturer: 'Steffan',
+    selectedCourse: this
+  },
+];
